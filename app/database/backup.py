be765213b8026505bd8c8412e1246c92c8e25c7e
PrivetLybimiy <PrivetLybimiy@yandex.ru>
2024-10-19 21:17:07 +0300

    .gitignore

--- app/database/backup.py
+++ app/database/backup.py
@@ -4,14 +4,12 @@ from datetime import datetime
 from sqlalchemy.ext.asyncio import create_async_engine
 from app.database.models import Base
 
-# Получение URL базы данных и пароля из переменных окружения
 db_url = os.getenv('DATABASE_URL')
 PGPASSWORD = os.getenv("PGPASSWORD")
 db = create_async_engine(url=db_url)
 
 BACKUP_DIR = r"C:/bd_backup"
 
-# Создание директории для резервных копий, если она не существует
 os.makedirs(BACKUP_DIR, exist_ok=True)
 
 async def backup_database():
